b88f4d3534385282970dca0b41c4b5b9963104a8
mahdi-shouib <mahdiomar402@gmail.com>
2025-08-27 22:29:10 +0300

    Added sharp package and basic resizing and displaying functionality

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import sharp from 'sharp';
 import path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.get('/api/images', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/../assets/thumb/`, 'thumb.jpg'));
+app.get('/api/images', async (req, res) => {
+
+    const filename = (req.query.filename as unknown) as string | undefined;
+    const width = (typeof req.query.width === 'string')? parseInt(req.query.width) : undefined;
+    const height = (typeof req.query.height === 'string')? parseInt(req.query.height) : undefined;
+    const newPath = `assets/thumb/${width}w_${height}h_${filename}`;
+
+    await sharp(`assets/full/${filename}`).resize(width, height).toFile(newPath)
+
+    res.sendFile(path.join(`${__dirname}/..`, newPath));
 });
 
 app.listen(port, () => {
