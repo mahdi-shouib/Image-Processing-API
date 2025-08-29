ba95bd16c5fe20c28a002a1e821041b4a086bffc
mahdi-shouib <mahdiomar402@gmail.com>
2025-08-29 21:39:36 +0300

    Added tests for deleting

--- src/tests/util/imageProcessSpec.ts
+++ src/tests/util/imageProcessSpec.ts
@@ -1,4 +1,5 @@
 import imageProcess from '../../util/imageProcess';
+import fs from 'fs/promises';
 
 describe('Testing image processing functions', () => {
 
@@ -14,13 +15,29 @@ describe('Testing image processing functions', () => {
 
 	describe('Test cached images exist or not', () => {
 		it('not real file is not cached', async () => {
-			expect(await imageProcess.IsImageCached('notreal.file', 100, 100)).toBe(false);
+			expect(await imageProcess.IsImageCached('notreal.file', 200, 200)).toBe(false);
 		});
 
 		it('cached image exists', async () => {
 			await imageProcess.ResizeImage('palmtunnel.jpg', 500, 500);
 			expect(await imageProcess.IsImageCached('palmtunnel.jpg', 500, 500)).toBe(true);
-			await imageProcess.DeleteCachedImage('palmtunnel.jpg');
+			await fs.unlink('./assets/thumb/500w_500h_palmtunnel.jpg');
+		});
+	});
+
+    describe('Test deleting images', () => {
+		it('deleting a non real file doesnt throw error', async () => {
+			expect(async () => { await imageProcess.DeleteCachedImage('notreal.file'); }).not.toThrow();
+		});
+
+		it('should delete all cached files', async () => {
+            const img = 'santamonica.jpg';
+			await imageProcess.ResizeImage(img, 1000, 500);
+			await imageProcess.ResizeImage(img, 800, 300);
+			await imageProcess.ResizeImage(img, 200, 700);
+            await imageProcess.DeleteCachedImage(img);
+            const files = (await fs.readdir('./assets/thumb')).filter(f => f.endsWith(img));
+			expect(files).toHaveSize(0);
 		});
 	});
 });
