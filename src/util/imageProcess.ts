5406e5b93135514617e4e105719740ca5f36b74b
mahdi-shouib <mahdiomar402@gmail.com>
2025-08-29 16:43:12 +0300

    Added resizing image function

--- src/util/imageProcess.ts
+++ src/util/imageProcess.ts
@@ -1,10 +1,21 @@
 import fs from 'fs';
+import sharp from 'sharp';
 
 const CheckFullImage = (filename: string): boolean => {
 	const imgPath = `assets/full/${filename}`;
 	return fs.existsSync(imgPath);
 };
 
+const ResizeImage = (filename: string, width: number, height: number): Promise<sharp.OutputInfo> | undefined => {
+
+    if(!CheckFullImage(filename) || width <= 0 || height <= 0) return;
+
+	const imgPath = `assets/full/${filename}`;
+	const newPath = `assets/thumb/${width}w_${height}h_${filename}`;
+	return sharp(imgPath).resize(width, height).toFile(newPath);
+}
+
 export = {
 	CheckFullImage,
+    ResizeImage
 };
