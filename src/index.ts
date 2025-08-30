ea2ac90f93590d93306429249013884d83417e2f
mahdi-shouib <mahdiomar402@gmail.com>
2025-08-30 17:04:42 +0300

    Added routes and mod index to use them

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import express from 'express';
-import path from 'path';
-import ValidateParameters from './util/paramValid';
+import routes from './routes';
 
 const app = express();
 const port = 3000;
 
-app.get('/api/images', ValidateParameters, async (req, res) => {
+app.get('/', (req, res) => {
+    res.send('Root route');
+})
 
-    const filename = req.query.filename as unknown as string | undefined;
-    const width = parseInt(req.query.width!.toString());
-    const height = parseInt(req.query.height!.toString());
-
-    const newPath = `assets/thumb/${width}w_${height}h_${filename}`;
-    res.sendFile(path.join(`${__dirname}/..`, newPath));
-});
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
