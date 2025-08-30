9ed3f6fbff06bf865536bf22152e46c0a143d616
mahdi-shouib <mahdiomar402@gmail.com>
2025-08-30 14:38:42 +0300

    Added endpoint tests

---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,33 @@
+import app from '../index';
+import supertest from 'supertest';
+
+const request = supertest(app);
+
+describe('Testing endpoints', () => {
+
+    describe('Test invalid requests', () => {
+        it('images endpoint with no parameters is missing filename bad request', async () => {
+            const response = await request.get('/api/images');
+            expect(response.badRequest).toBe(true);
+            expect(response.text).toEqual('Error: missing filename parameter');
+        })
+
+        it('images endpoint with no width and height is missing width or height bad request', async () => {
+            const response = await request.get('/api/images?filename=img.jpg');
+            expect(response.badRequest).toBe(true);
+            expect(response.text).toEqual('Error: missing width or height parameters');
+        })
+
+        it('images endpoint with only no width is missing width or height bad request', async () => {
+            const response = await request.get('/api/images?filename=img.jpg&height=500');
+            expect(response.badRequest).toBe(true);
+            expect(response.text).toEqual('Error: missing width or height parameters');
+        })
+
+        it('images endpoint with only no height is missing width or height bad request', async () => {
+            const response = await request.get('/api/images?filename=img.jpg&width=500');
+            expect(response.badRequest).toBe(true);
+            expect(response.text).toEqual('Error: missing width or height parameters');
+        })
+    })
+})
