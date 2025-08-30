1e4f9487044d96f342d8a7257c3e5b7d0c9e3c57
mahdi-shouib <mahdiomar402@gmail.com>
2025-08-30 16:31:24 +0300

    Fixed and added all validations

--- src/util/paramValid.ts
+++ src/util/paramValid.ts
@@ -1,19 +1,28 @@
 import express from 'express';
+import imageProcess from './imageProcess';
 
-const ValidateParameters = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+const ValidateParameters = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
 
-    const filename = req.query.filename;
+    const filename = (req.query.filename as unknown) as string | undefined;
     if (!filename)
         return res.status(400).send('Error: missing filename parameter');
+
+    if (!req.query.width || !req.query.height)
+        return res.status(400).send('Error: missing width or height parameters');
 
     const width = parseInt(req.query.width!.toString());
     const height = parseInt(req.query.height!.toString());
 
-    if (!req.query.width || !req.query.height)
-        res.status(400).send('Error: missing width or height parameters');
-
     if (width <= 0 || height <= 0 || Number.isNaN(width) || Number.isNaN(height))
-        res.status(400).send('Error: invalid width or height parameters');
+        return res.status(400).send('Error: invalid width or height parameters');
+
+    if (!await imageProcess.IsImageCached(filename, width, height)) {
+        try {
+            await imageProcess.ResizeImage(filename, width, height)
+        } catch (err) {
+            return res.status(404).send((err as object).toString());
+        }
+    }
 
     next();
 }
