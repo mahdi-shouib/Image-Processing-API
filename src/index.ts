09fddeb8f23269472068c094bdfaac766bb6f41d
mahdi-shouib <mahdiomar402@gmail.com>
2025-08-30 16:31:52 +0300

    Used the validation middleware

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,15 @@
 import express from 'express';
 import path from 'path';
-import imgProcess from './util/imageProcess';
+import ValidateParameters from './util/paramValid';
 
 const app = express();
 const port = 3000;
 
-app.get('/api/images', async (req, res) => {
+app.get('/api/images', ValidateParameters, async (req, res) => {
 
-    const filename = (req.query.filename as unknown) as string | undefined;
-
-    if (!filename) {
-        res.status(400).send('Error: missing filename parameter');
-        return;
-    } else if (!req.query.width || !req.query.height) {
-        res.status(400).send('Error: missing width or height parameters');
-        return;
-    }
-
+    const filename = req.query.filename as unknown as string | undefined;
     const width = parseInt(req.query.width!.toString());
     const height = parseInt(req.query.height!.toString());
-
-    if (width <= 0 || height <= 0 || Number.isNaN(width) || Number.isNaN(height)) {
-        res.status(400).send('Error: invalid width or height parameters');
-        return;
-    }
-
-    if (!await imgProcess.IsImageCached(filename, width, height)) {
-        try {
-            await imgProcess.ResizeImage(filename, width, height)
-        } catch(err) {
-            res.status(404).send((err as object).toString());
-            return;
-        }
-    }
 
     const newPath = `assets/thumb/${width}w_${height}h_${filename}`;
     res.sendFile(path.join(`${__dirname}/..`, newPath));
